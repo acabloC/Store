8a80742dda614d3bfaac5b73c729730e40433e59
G_EST <aaa>
2024-03-17 17:32:18 +1000

    d23 proverit home

--- d18/users/urls.py
+++ d18/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 app_name = "users"
-urlpatterns = [path('login/', login, name = "login"),
-path('register/', register, name = "register")]
+urlpatterns = [
+    path('login/', login, name = "login"),
+    path('register/', register, name = "register")
+]
 
--- d18/users/views.py
+++ d18/users/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from d18.users.forms import UserLoginForm
+from d18.users.models import User
 
-# Create your views here.
+from django.contrib import auth
+
+
+def Login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+
+
+        if form.is_valid():
+
+            username = request.POST[ "username"]
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+
+    else:
+        form = UserLoginForm()
+        context = {"form" : form}
+
+    return render(request, 'users / login.html', context = context)
